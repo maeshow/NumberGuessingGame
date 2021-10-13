df88ea7ec7743693017a772680042a8def9ab2d4
maeshow <necobudou3011@gmail.com>
2021-10-13 16:15:28 +0900

    add Messages

--- src/App.java
+++ src/App.java
@@ -10,37 +10,37 @@ public class App {
         Random random = new Random();
         int randomNumber = random.nextInt(MAX_RANDOM_NUMBER_AMOUNT);
 
-        System.out.println("数字を当ててみてね");
+        System.out.println(Messages.GAME_DESCRIPTION);
         System.out.println();
-        System.out.printf("答えられるのは%d回までだよ", RESPONSE_LIMIT);
+        System.out.printf(Messages.ANSWER_LIMIT_DESCRIPTION, RESPONSE_LIMIT);
         System.out.println();
 
         try {
             boolean isComplete = false;
             Scanner scanner = new Scanner(System.in);
             for (int i = 0; i < RESPONSE_LIMIT; i++) {
-                System.out.printf("%d回目: ", i + 1);
+                System.out.printf(Messages.ANSWER_COUNT, i + 1);
                 Integer inputNumber = scanner.nextInt();
 
                 if (randomNumber == inputNumber) {
-                    System.out.printf("すごい！！%d回で当てられちゃった！", i + 1);
+                    System.out.printf(Messages.COMPLETE_GAME, i + 1);
                     isComplete = true;
                     break;
                 } else if (randomNumber > inputNumber) {
-                    System.out.println("もっと大きい数だよ");
+                    System.out.println(Messages.BIGGER_TIPS);
                 } else if (randomNumber < inputNumber) {
-                    System.out.println("もっと小さい数だよ");
+                    System.out.println(Messages.SMALLER_TIPS);
                 }
             }
 
             if (!isComplete) {
-                System.out.printf("残念！！正解は %d でした！", randomNumber);
+                System.out.printf(Messages.GAME_OVER, randomNumber);
             }
 
             scanner.close();
 
         } catch (InputMismatchException e) {
-            System.out.println("数字を入力してください");
+            System.out.println(Messages.ENTER_NUMBER_WARN);
         }
     }
 }
---
+++ src/Messages.java
@@ -0,0 +1,11 @@
+public class Messages {
+    public static final String GAME_DESCRIPTION = "数字を当ててみてね";
+    public static final String ANSWER_LIMIT_DESCRIPTION = "答えられるのは%d回までだよ";
+    public static final String ANSWER_COUNT = "%d回目： ";
+    public static final String COMPLETE_GAME = "すごい！！%d回で当てられちゃった！";
+    public static final String BIGGER_TIPS = "もっと大きい数だよ";
+    public static final String SMALLER_TIPS = "もっと小さい数だよ";
+    public static final String GAME_OVER = "残念！！正解は %d でした！";
+
+    public static final String ENTER_NUMBER_WARN = "数字を入力してください";
+}
